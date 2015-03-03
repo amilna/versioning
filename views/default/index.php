385b7565d257905b94565f0d9719da43f7f3b1ef
Satrio Arditama <iyo@amilna.com>
2015-03-03 13:55:15 +0700

    update

--- views/default/index.php
+++ views/default/index.php
@@ -1,12 +1,38 @@
-<div class="versioning-default-index">
-    <h1><?= $this->context->action->uniqueId ?></h1>
-    <p>
-        This is the view content for action "<?= $this->context->action->id ?>".
-        The action belongs to the controller "<?= get_class($this->context) ?>"
-        in the "<?= $this->context->module->id ?>" module.
-    </p>
-    <p>
-        You may customize this page by editing the following file:<br>
-        <code><?= __FILE__ ?></code>
-    </p>
+<?php
+use yii\helpers\Html;
+use yii\helpers\Url;
+
+?>
+<div class="blog-default-index">
+    
+    <div class="jumbotron">
+		<h2>Yii2 Extensions for Data Versioning</h2>
+        <h1>Congratulations!</h1>
+        
+
+        <p class="lead">You have successfully installed Data Versioning extension for your Yii-powered application.</p>
+
+        <p><?= Html::a(Yii::t('app','Get start to manage group'),['//versioning/group'],["class"=>"btn btn-lg btn-success"])?></p>
+    </div>
+
+    <div class="body-content">
+
+        <div class="row">
+            <div class="col-md-6">
+                <h2>Record</h2>
+
+                <p>Representation of single, implicitly structured data item in a table. In simple terms, a database table can be thought of as consisting of rows and columns or fields.[1] Each row in a table represents a set of related data, and every row in the table has the same structure.</p>
+
+                <p><?= Html::a(Yii::t('app','Manage Records'),['//versioning/record'],["class"=>"btn btn-primary"])?></p>
+            </div>
+            <div class="col-md-6">
+                <h2>Version</h2>
+
+                <p>The management of changes to data, and other collections of information, since the begining of record creation.</p>
+
+                <p><?= Html::a(Yii::t('app','Manage Versions'),['//versioning/version'],["class"=>"btn btn-primary"])?></p>
+            </div>            
+        </div>
+
+    </div>
 </div>
